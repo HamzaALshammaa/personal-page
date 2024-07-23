da71484745a3082ca213212c30a3030f9ace213e
HamzaALshammaa <hamza9ayman@gmail.com>
2024-07-23 23:57:10 +0300

    fix: fixing the links

--- src/App.js
+++ src/App.js
@@ -1,11 +1,5 @@
 import React from 'react';
 import Header from './components/Header';
-import About from './components/About';
-import Experience from './components/Experience';
-import Education from './components/Education';
-import Skills from './components/Skills';
-import Contact from './components/Contact';
-import Footer from './components/Footer';
 import './App.css';
 
 function App() {
@@ -13,13 +7,8 @@ function App() {
     <div className="App">
       <Header />
       <main>
-        <About />
-        <Experience />
-        <Education />
-        <Skills />
-        <Contact />
+        <h1>Hello, World!</h1>
       </main>
-      <Footer />
     </div>
   );
 }
