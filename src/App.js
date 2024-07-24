240edf0b393b1d7d2907107ebd55b3fdc7e65e5e
HamzaALshammaa <hamza9ayman@gmail.com>
2024-07-24 12:07:47 +0300

    test

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import React from 'react';
+import Header from './components/Header';
 import About from './components/About';
+import Experience from './components/Experience';
+import Education from './components/Education';
+import Skills from './components/Skills';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <About />
+      <Header />
+      <main>
+        <About />
+        <Experience />
+        <Education />
+        <Skills />
+        <Contact />
+      </main>
+      <Footer />
     </div>
   );
 }
