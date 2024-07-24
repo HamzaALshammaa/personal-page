4b8cae0bbd71990ea19199a4aeb548e5932aa767
HamzaALshammaa <hamza9ayman@gmail.com>
2024-07-24 12:01:45 +0300

    test

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import Header from './components/Header';
+import About from './components/About';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Header />
+      <About />
     </div>
   );
 }
